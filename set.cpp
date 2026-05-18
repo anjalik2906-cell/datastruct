df57d8d56a2a9daebdc0a80176fc6b9bda00306b
Anjali <anjalik.2906@gmail.com>
2026-05-18 09:44:46 +0530

    commit

--- set.cpp
+++ set.cpp
@@ -2,12 +2,18 @@
 #include<set>
 using namespace std;
 int main(){
-    set<int> s;
+    set<int> s={1,2,3,4};
     s.insert(5);
     s.insert(10);
     s.insert(15);
     for(const auto& elem : s){
-        cout << elem << " ";
+        cout << elem << " "<<endl;
+    }
+    if(s.find(10) != s.end()){
+        cout <<"element found"<<endl;
+    }
+    else{
+        cout<<"element not found"<<endl;
     }
     return 0;
 }
