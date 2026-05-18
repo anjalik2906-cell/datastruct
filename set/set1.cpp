1475c3c5dd058559539f094c1eba68a254188888
Anjali <anjalik.2906@gmail.com>
2026-05-18 10:05:01 +0530

    set1.cpp

--- set/set1.cpp
+++ set/set1.cpp
@@ -1,22 +1,41 @@
-#include<iostream>
-#include<set>
+// #include<iostream>
+// #include<set>
+// using namespace std;
+// //addition of numbers using set
+
+// int main() {
+//     set<int> s;
+//     int a, b;
+
+//     cout << "Enter first number: ";
+//     cin >> a;
+
+//     cout << "Enter second number: ";
+//     cin >> b;
+
+//     s.insert(a);
+//     s.insert(b);
+
+//     cout << "Addition = " << a + b;
+
+//     return 0;
+// }
+#include <iostream>
+#include <set>
+#include <numeric>
 using namespace std;
-//addition of numbers using set
+int main(){
+    set<int> a;
+    int n;
+    cout<<"enter size of set: "<<endl;
+    cin>>n;
+     cout<<"enter element: "<<endl;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        a.insert(x);
+    }
+    int sum = accumulate(a.begin(),a.end(),0);
+    cout<<"sum of all set: "<<sum<<endl;
 
-int main() {
-    set<int> s;
-    int a, b;
-
-    cout << "Enter first number: ";
-    cin >> a;
-
-    cout << "Enter second number: ";
-    cin >> b;
-
-    s.insert(a);
-    s.insert(b);
-
-    cout << "Addition = " << a + b;
-
-    return 0;
-}
+    }
