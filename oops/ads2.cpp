32a57041419d9cd0f57ad5781fb96f5e973b6e1f
Anjali <anjalik.2906@gmail.com>
2026-09-21 11:01:12 +0530

    modified:   oops/ads2.cpp

--- oops/ads2.cpp
+++ oops/ads2.cpp
@@ -1,3 +1,35 @@
-#include<iostream>
+
+#include <iostream>
 using namespace std;
-//recursion function
+
+class Student {
+    string name;
+    int age;
+
+public:
+    // Constructor
+    Student(string n, int a) {
+        name = n;
+        age = a;
+        cout << "Constructor called" << endl;
+    }
+
+    // Function
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+
+    // Destructor
+    ~Student() {
+        cout << "Destructor called" << endl;
+    }
+};
+
+int main() {
+    Student s1("Anjali", 19);
+
+    s1.display();
+
+    return 0;
+}
